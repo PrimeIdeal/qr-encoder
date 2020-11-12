bd48759adac2c0b2f6937d54d3e997d1d7cf0abd
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-11-11 21:16:42 -0700

    initial structure for encoder classes

---
+++ encode/data_encoder.py
@@ -0,0 +1,46 @@
+class qr_encoder:
+
+    def __init__(self):
+        pass
+
+    def get_version(self):
+        pass
+
+    def get_num_bits(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def postprocess(self):
+        pass
+
+    def correct_error(self):
+        pass
+
+
+class numeric_encoder(qr_encoder):
+
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+
+class alphanumeric_encoder(qr_encoder):
+
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+
+class bytes_encoder(qr_encoder):
+
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
