24a2b7b556c431afaeb7ec95f87f3b8d10b1bc0c
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-11-17 23:39:35 -0700

    added docstrings

--- encode/data_encoder.py
+++ encode/data_encoder.py
@@ -2,12 +2,38 @@ from encode.common import CHAR_CAP
 
 
 class qr_encoder:
+    """
+    QR Encoder object.
+    """
 
     def __init__(self, message: str, correction_level: str):
+        """
+        Constructor for the qr_encoder class.
+
+        Parameters
+        ----------
+        message : str
+            The message to be encoded.
+        correction_level : str
+            Error correction level for the QR code.
+        """
         self.message = message
         self.correction_level = correction_level
 
-    def get_version(self):
+    def get_version(self) -> int:
+        """
+        Determines the smallest possible format for the QR code.
+
+        Returns
+        -------
+        int
+            QR format version.
+
+        Raises
+        ------
+        ValueError
+            Message is too long.
+        """
         mode = self._get_mode()
 
         for idx, cap in enumerate(CHAR_CAP[mode][self.correction_level]):
@@ -35,6 +61,9 @@ class qr_encoder:
 
 
 class numeric_encoder(qr_encoder):
+    """
+    QR Encoder using numeric encoding mode.
+    """
 
     def _get_mode(self):
         return 'numeric'
@@ -44,6 +73,9 @@ class numeric_encoder(qr_encoder):
 
 
 class alphanumeric_encoder(qr_encoder):
+    """
+    QR Encoder using alphanumeric encoding mode.
+    """
 
     def _get_mode(self):
         return 'alphanumeric'
@@ -53,6 +85,9 @@ class alphanumeric_encoder(qr_encoder):
 
 
 class bytes_encoder(qr_encoder):
+    """
+    QR Encoder using bytes encoding mode.
+    """
 
     def _get_mode(self):
         return 'bytes'
