473e6bb1f75945b01ef251b04583ac6718f32464
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-11-17 23:26:32 -0700

    moved functions to new file to avoid circular import

--- encode/common.py
+++ encode/common.py
@@ -1,89 +1,24 @@
-from encode.data_encoder import (
-    alphanumeric_encoder,
-    bytes_encoder,
-    numeric_encoder,
-    qr_encoder
-)
-
-
-def select_encoding(msg: str) -> str:
-    """
-    Determines which type of text encoding will be used for the message.
-
-    If msg is strictly decimal, specifies numeric encoding. If msg is composed
-    of numbers, uppercase letters, and characters in
-    (' ', '$', '%', '*', '+', '-', '.', '/', ':'), specifies alphanumeric
-    encoding. Otherwise, specifies byte encoding.
-
-    Parameters
-    ----------
-    msg : str
-        Text to be encoded as a QR code.
-
-    Returns
-    -------
-    str
-        Specified encoding mode.
-
-    Raises
-    ------
-    ValueError
-        Input is too long to be encoded.
-    """
-    if len(msg) > 7089:
-        raise ValueError('Input exceeds maximum encoding length.')
-
-    if msg.isdecimal():
-        return 'numeric'
-
-    for char in msg:
-        if (char.isalpha() and char.islower()) or \
-            (not char.isalpha() and not char.isdecimal()
-             and char not in ALPHANUMERIC_CHARS):
-            return 'byte'
-
-    return 'alphanumeric'
-
-
-def select_encoder(msg: str, correction_level: str) -> qr_encoder:
-    """
-    Selects an appropriate QR encoder for the message input.
-
-    Parameters
-    ----------
-    msg: str
-        Text to be encoded as a QR code.
-    correction_level : str
-        Specified error correction level.
-
-    Returns
-    -------
-    qr_encoder
-        qr_encoder object for the message.
-
-    Raises
-    ------
-    TypeError
-        Input is not a string.
-    ValueError
-        Correction level not in ('L', 'M', 'Q', 'H')
-    """
-    if not isinstance(msg, str):
-        raise TypeError(f'Message is not a string: {msg}.')
-    if correction_level.upper() not in CORRECTION_LEVELS:
-        raise ValueError(f'Unrecognized correction level: {correction_level}.')
-
-    encoding = select_encoding(msg)
-    if encoding == 'numeric':
-        return numeric_encoder(msg, correction_level.upper())
-    if encoding == 'alphanumeric':
-        return alphanumeric_encoder(msg, correction_level.upper())
-    else:
-        return bytes_encoder(msg, correction_level.upper())
-
-
 ALPHANUMERIC_CHARS = {' ', '$', '%', '*', '+', '-', '.', '/', ':'}
 
 CORRECTION_LEVELS = {'L', 'M', 'Q', 'H'}
 
-CHAR_CAP = {}
+CHAR_CAP = {
+    'numeric': {
+        'L': [41, 77, 127, 187, 255, 322, 370, 461, 552, 652],
+        'M': [34, 63, 101, 149, 202, 255, 293, 365, 432, 513],
+        'Q': [27, 48, 77, 111, 144, 178, 207, 259, 312, 364],
+        'H': [17, 34, 58, 82, 106, 139, 154, 202, 235, 288]
+    },
+    'alphanumeric': {
+        'L': [25, 47, 77, 114, 154, 195, 224, 279, 335, 395],
+        'M': [20, 38, 61, 90, 122, 154, 178, 221, 262, 311],
+        'Q': [16, 29, 47, 67, 87, 108, 125, 157, 189, 221],
+        'H': [10, 20, 35, 50, 64, 84, 93, 122, 143, 174]
+    },
+    'bytes': {
+        'L': [17, 32, 53, 78, 106, 134, 154, 192, 230, 271],
+        'M': [14, 26, 42, 62, 84, 106, 122, 152, 180, 213],
+        'Q': [11, 20, 32, 46, 60, 74, 86, 108, 130, 151],
+        'H': [7, 14, 24, 34, 44, 58, 64, 84, 98, 119]
+    }
+}
---
+++ encode/preliminary.py
@@ -0,0 +1,86 @@
+from encode.common import (
+    ALPHANUMERIC_CHARS,
+    CORRECTION_LEVELS
+)
+from encode.data_encoder import (
+    alphanumeric_encoder,
+    bytes_encoder,
+    numeric_encoder,
+    qr_encoder
+)
+
+
+def select_encoding(msg: str) -> str:
+    """
+    Determines which type of text encoding will be used for the message.
+
+    If msg is strictly decimal, specifies numeric encoding. If msg is composed
+    of numbers, uppercase letters, and characters in
+    (' ', '$', '%', '*', '+', '-', '.', '/', ':'), specifies alphanumeric
+    encoding. Otherwise, specifies byte encoding.
+
+    Parameters
+    ----------
+    msg : str
+        Text to be encoded as a QR code.
+
+    Returns
+    -------
+    str
+        Specified encoding mode.
+
+    Raises
+    ------
+    ValueError
+        Input is too long to be encoded.
+    """
+    if len(msg) > 7089:
+        raise ValueError('Input exceeds maximum encoding length.')
+
+    if msg.isdecimal():
+        return 'numeric'
+
+    for char in msg:
+        if (char.isalpha() and char.islower()) or \
+            (not char.isalpha() and not char.isdecimal()
+             and char not in ALPHANUMERIC_CHARS):
+            return 'byte'
+
+    return 'alphanumeric'
+
+
+def select_encoder(msg: str, correction_level: str) -> qr_encoder:
+    """
+    Selects an appropriate QR encoder for the message input.
+
+    Parameters
+    ----------
+    msg: str
+        Text to be encoded as a QR code.
+    correction_level : str
+        Specified error correction level.
+
+    Returns
+    -------
+    qr_encoder
+        qr_encoder object for the message.
+
+    Raises
+    ------
+    TypeError
+        Input is not a string.
+    ValueError
+        Correction level not in ('L', 'M', 'Q', 'H')
+    """
+    if not isinstance(msg, str):
+        raise TypeError(f'Message is not a string: {msg}.')
+    if correction_level.upper() not in CORRECTION_LEVELS:
+        raise ValueError(f'Unrecognized correction level: {correction_level}.')
+
+    encoding = select_encoding(msg)
+    if encoding == 'numeric':
+        return numeric_encoder(msg, correction_level.upper())
+    if encoding == 'alphanumeric':
+        return alphanumeric_encoder(msg, correction_level.upper())
+    else:
+        return bytes_encoder(msg, correction_level.upper())
