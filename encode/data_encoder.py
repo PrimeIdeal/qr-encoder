6d4e1f30915ed43a97abbd59fea3d9636db7206d
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-11-17 23:25:53 -0700

    added get_version method

--- encode/data_encoder.py
+++ encode/data_encoder.py
@@ -1,3 +1,6 @@
+from encode.common import CHAR_CAP
+
+
 class qr_encoder:
 
     def __init__(self, message: str, correction_level: str):
@@ -5,7 +8,15 @@ class qr_encoder:
         self.correction_level = correction_level
 
     def get_version(self):
-        pass
+        mode = self._get_mode()
+
+        for idx, cap in enumerate(CHAR_CAP[mode][self.correction_level]):
+            if len(self.message) <= cap:
+                return idx+1
+
+        raise ValueError(
+            'Message is too long for specified error correction level.'
+        )
 
     def get_num_bits(self):
         pass
@@ -19,11 +30,14 @@ class qr_encoder:
     def correct_error(self):
         pass
 
+    def _get_mode(self):
+        return 'kanji'
+
 
 class numeric_encoder(qr_encoder):
 
-    def __init__(self, message: str, correction_level: str):
-        qr_encoder.__init__(self, message, correction_level)
+    def _get_mode(self):
+        return 'numeric'
 
     def encode(self):
         pass
@@ -31,8 +45,8 @@ class numeric_encoder(qr_encoder):
 
 class alphanumeric_encoder(qr_encoder):
 
-    def __init__(self, message: str, correction_level: str):
-        qr_encoder.__init__(self, message, correction_level)
+    def _get_mode(self):
+        return 'alphanumeric'
 
     def encode(self):
         pass
@@ -40,8 +54,8 @@ class alphanumeric_encoder(qr_encoder):
 
 class bytes_encoder(qr_encoder):
 
-    def __init__(self, message: str, correction_level: str):
-        qr_encoder.__init__(self, message, correction_level)
+    def _get_mode(self):
+        return 'bytes'
 
     def encode(self):
         pass
