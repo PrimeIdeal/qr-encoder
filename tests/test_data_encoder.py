073fe999541f42c94452f7601ae59f9650652a72
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-11-20 23:59:22 -0700

    reworked constructor tests to take advantage of encoder fixtures

--- tests/test_data_encoder.py
+++ tests/test_data_encoder.py
@@ -10,26 +10,24 @@ from encode.data_encoder import (
 class TestConstructor:
 
     @pytest.mark.parametrize(
-        'test_len, test_ec, version, cap',
+        'numeric_zeros, version, cap',
         [
-            (41, 'L', 1, 41),
-            (432, 'M', 9, 432),
-            (1932, 'Q', 27, 1933),
-            (2734, 'H', 38, 2735)
+            ((41, 'L'), 1, 41),
+            ((432, 'M'), 9, 432),
+            ((1932, 'Q'), 27, 1933),
+            ((2734, 'H'), 38, 2735)
         ],
         ids=[
             'Equal to cap - EC level L',
             'Equal to cap - EC level M',
             'Less than cap - EC level Q',
             'Less than cap - EC level H'
-        ]
+        ],
+        indirect=['numeric_zeros']
     )
-    def test_numeric_valid_msg(self, test_len, test_ec, version, cap):
-        test_msg = ''.join('0' for _ in range(test_len))
-        test_encoder = numeric_encoder(test_msg, test_ec)
-
-        assert test_encoder.version == version
-        assert test_encoder.bit_cap == cap
+    def test_numeric_valid_msg(self, numeric_zeros, version, cap):
+        assert numeric_zeros.version == version
+        assert numeric_zeros.bit_cap == cap
 
     def test_numeric_invalid_msg(self):
         test_msg, test_ec = ''.join('4' for _ in range(3599)), 'H'
@@ -41,26 +39,24 @@ class TestConstructor:
         assert str(error_info.value) == error_msg
 
     @pytest.mark.parametrize(
-        'test_len, test_ec, version, cap',
+        'alphanumeric_zeros, version, cap',
         [
-            (25, 'L', 1, 25),
-            (262, 'M', 9, 262),
-            (1171, 'Q', 27, 1172),
-            (1657, 'H', 38, 1658)
+            ((25, 'L'), 1, 25),
+            ((262, 'M'), 9, 262),
+            ((1171, 'Q'), 27, 1172),
+            ((1657, 'H'), 38, 1658)
         ],
         ids=[
             'Equal to cap - EC level L',
             'Equal to cap - EC level M',
             'Less than cap - EC level Q',
             'Less than cap - EC level H'
-        ]
+        ],
+        indirect=['alphanumeric_zeros']
     )
-    def test_alphanumeric_valid_msg(self, test_len, test_ec, version, cap):
-        test_msg = ''.join('0' for _ in range(test_len))
-        test_encoder = alphanumeric_encoder(test_msg, test_ec)
-
-        assert test_encoder.version == version
-        assert test_encoder.bit_cap == cap
+    def test_alphanumeric_valid_msg(self, alphanumeric_zeros, version, cap):
+        assert alphanumeric_zeros.version == version
+        assert alphanumeric_zeros.bit_cap == cap
 
     def test_alphanumeric_invalid_msg(self):
         test_msg, test_ec = ''.join('4' for _ in range(2181)), 'H'
@@ -72,26 +68,24 @@ class TestConstructor:
         assert str(error_info.value) == error_msg
 
     @pytest.mark.parametrize(
-        'test_len, test_ec, version, cap',
+        'bytes_zeros, version, cap',
         [
-            (17, 'L', 1, 17),
-            (180, 'M', 9, 180),
-            (804, 'Q', 27, 805),
-            (1138, 'H', 38, 1139)
+            ((17, 'L'), 1, 17),
+            ((180, 'M'), 9, 180),
+            ((804, 'Q'), 27, 805),
+            ((1138, 'H'), 38, 1139)
         ],
         ids=[
             'Equal to cap - EC level L',
             'Equal to cap - EC level M',
             'Less than cap - EC level Q',
             'Less than cap - EC level H'
-        ]
+        ],
+        indirect=['bytes_zeros']
     )
-    def test_bytes_valid_msg(self, test_len, test_ec, version, cap):
-        test_msg = ''.join('0' for _ in range(test_len))
-        test_encoder = bytes_encoder(test_msg, test_ec)
-
-        assert test_encoder.version == version
-        assert test_encoder.bit_cap == cap
+    def test_bytes_valid_msg(self, bytes_zeros, version, cap):
+        assert bytes_zeros.version == version
+        assert bytes_zeros.bit_cap == cap
 
     def test_bytes_invalid_msg(self):
         test_msg, test_ec = ''.join('4' for _ in range(1499)), 'H'
